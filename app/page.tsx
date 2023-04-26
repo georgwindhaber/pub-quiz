0919bc005fe01d2b2447306a6fe63b5831660c2e
georgwindhaber <georg.windhaber@gmail.com>
2023-04-26 18:25:47 +0200

    Add more questions

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,18 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
+type Category = "Anzahl" | "Distanz";
+
 type Question = {
   id: number;
   question: string;
   answer: number;
+  annotation?: string;
+  annotationAnswer?: string;
   pointZonePercentage: number;
+  sourceUrl: string;
+  lastChangeOfData: string;
+  category: Category;
 };
 
 type Team = {
@@ -16,30 +23,118 @@ type Team = {
   answers: number[];
 };
 
+const POINT_ZONE_PERCENTAGE_DEFAULT = 50;
+
 const questions: Question[] = [
   {
     id: 1,
-    question: "What is the capital of France?",
-    answer: 50,
-    pointZonePercentage: 50,
+    question:
+      "Stand 2021 hatte die Stadt Wien die meisten Daten auf OpenData.gv.at veröffentlicht, mit insgesamt 551 Datensätzen. Die Gemeinde Engerwitzdorf veröffentlichte am zweit meisten Datensätze. Wie viele waren es?",
+    answer: 514,
+    pointZonePercentage: POINT_ZONE_PERCENTAGE_DEFAULT,
+    sourceUrl: "https://drawingdata.net/datagvat/",
+    lastChangeOfData: "18. November 2021",
+    category: "Anzahl",
   },
   {
     id: 2,
-    question: "What is the capital of Germany?",
-    answer: 100,
-    pointZonePercentage: 25,
+    question:
+      "Es gibt in Wien 1037 Ampeln mit Akustikkennung. Wie viele gibt es OHNE Akustikkennung?",
+    answer: 298,
+    pointZonePercentage: POINT_ZONE_PERCENTAGE_DEFAULT,
+    sourceUrl:
+      "https://www.data.gv.at/katalog/dataset/c5cf2502-7572-4fd1-a836-48b335a2d47d#resources",
+    lastChangeOfData: "15. April 2020",
+    category: "Anzahl",
   },
   {
     id: 3,
-    question: "What is the capital of Italy?",
-    answer: 100,
-    pointZonePercentage: 12.5,
+    question: "Es wie Viele Werbeflächen gibt es in Wien?",
+    answer: 18703,
+    annotation:
+      "Typische Werbeflächen, die sehr häufig im Stadtbild und auf öffentlichem Gut anzutreffen sind: - Rolling Boards - Plakatwände - City Lights in Wartehallen oder Telefonzellen - City Lights frei stehend - Leuchtsäulen - Litfaßsäulen (Kulturwerbung) - Miniposter (Halbschalen) an Lichtmasten (Kulturwerbung) - Flachschilder auf Lichtmasten",
+    pointZonePercentage: POINT_ZONE_PERCENTAGE_DEFAULT,
+    sourceUrl:
+      "https://www.data.gv.at/katalog/dataset/82c36af8-4a5c-4c01-8df7-c19efd5ba7d9#resources",
+    lastChangeOfData: "24. April 2023",
+    category: "Anzahl",
   },
   {
     id: 4,
-    question: "What is the capital of Spain?",
-    answer: 100,
-    pointZonePercentage: 6.25,
+    question:
+      '"Nimm ein Sackerl fürs Gackerl" steht meistens oben: Wie viele Hundekotsackerlspender gibt es in Wien?',
+    answer: 4043,
+    pointZonePercentage: POINT_ZONE_PERCENTAGE_DEFAULT,
+    sourceUrl:
+      "https://www.data.gv.at/katalog/dataset/98908792-f315-4834-9b5e-20fed66cbe5a#resources",
+    lastChangeOfData: "15. April 2020",
+    category: "Anzahl",
+  },
+  {
+    id: 5,
+    question:
+      "Ziemlich fürn Arsch: Wie viele Rollen Klopapier (2 lagig, Kleinrolle) kaufte die Stadt Wien im Herbst 2022?",
+    answer: 610000,
+    pointZonePercentage: POINT_ZONE_PERCENTAGE_DEFAULT,
+    sourceUrl: "https://www.wien.gv.at/Vergabeportal/Detail/130519",
+    lastChangeOfData: "30. Juni 2022",
+    category: "Anzahl",
+  },
+  {
+    id: 6,
+    question:
+      "Wie weit ist der nähste Spielplatz vom Stephansplatz entfernt? (In Meter, Luftlinie)",
+    answer: 649,
+    annotationAnswer: "Es ist der Spielplatz 'Rudolfspark' im 1. Bezirk",
+    pointZonePercentage: POINT_ZONE_PERCENTAGE_DEFAULT,
+    sourceUrl:
+      "https://www.data.gv.at/katalog/dataset/bd8b518b-d812-46f9-b367-4c1b660cfc99",
+    lastChangeOfData: "15. April 2020",
+    category: "Distanz",
+  },
+  {
+    id: 7,
+    question: "Wie viele Wörter hat Karl Marx' 'Das Kapital' (Band 1)?",
+    answer: 196172,
+    annotation:
+      "Das Kapital. Kritik der politischen Ökonomie. Von Karl Marx. Erster Band. Buch I: Der Produktionsprocess des Kapitals. (Hamburg: Otto Meissner, 1867; New-York: L. W. Schmidt).",
+    pointZonePercentage: POINT_ZONE_PERCENTAGE_DEFAULT,
+    sourceUrl: "https://oll.libertyfund.org/page/marx-k1-1867",
+    lastChangeOfData: "27. Juni 2018",
+    category: "Anzahl",
+  },
+  {
+    id: 8,
+    question:
+      "Im Jahr 2019 lag das durchschnittliche Alter der Mutter zum Zeitpunkt der Geburt bei 31,2 Jahren. Wie hoch war das durchschnittliche Alter im Jahr 1989?",
+    answer: 26.8,
+    pointZonePercentage: 30,
+    sourceUrl:
+      "https://oesterreich.orf.at/stories/3066164/#:~:text=Das%20durchschnittliche%20Alter%20der%20Mutter,auf%20aktuell%2029%2C9%20Jahre",
+    lastChangeOfData: "10. September 2020",
+    category: "Anzahl",
+  },
+  {
+    id: 9,
+    question:
+      "Wie viele Steuermittel in Milliarden Euro sind von Staatshilfen für Corona, Energie und Teurung an UNTERNEHMEN geflossen?",
+    answer: 28.7,
+    pointZonePercentage: 30,
+    sourceUrl:
+      "https://www.derstandard.at/story/2000143809538/trotz-wirtschaftsbooms-staatshilfen-in-milliardenhoehe",
+    lastChangeOfData: "22. Februar 2023",
+    category: "Anzahl",
+  },
+  {
+    id: 10,
+    question:
+      "Wie oft wurde die 'Pille danach' in Deutschland im Jahr 2017 verkauft?",
+    answer: 808000,
+    pointZonePercentage: POINT_ZONE_PERCENTAGE_DEFAULT,
+    sourceUrl:
+      "https://www.spiegel.de/panorama/pille-danach-verkaufszahlen-sind-gestiegen-seit-sie-rezeptfrei-ist-a-00000000-0003-0001-0000-000002710996",
+    lastChangeOfData: "06. August 2018",
+    category: "Anzahl",
   },
 ];
 
